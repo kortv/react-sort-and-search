3ad6c463082ac4be07fbbf4b0f7cdc8a41071001
kortv <2mothy@gmail.com>
2016-03-28 20:45:31 +0300

    fix dependency

--- js/components/UserData.jsx
+++ js/components/UserData.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UserData({ handleClick, user }) {
   const imgSrc = `images/${user.image}.svg`;
   return (
-    <tr onClick = {handleClick} data-id={user.id}>
+    <tr onClick={handleClick} data-id={user.id}>
       <td data-id={user.id}><img src={imgSrc} /></td>
       <td data-id={user.id}>{ user.name }</td>
       <td data-id={user.id}>{ user.age }</td>
